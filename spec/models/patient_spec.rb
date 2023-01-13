e14611f70ce62e5ae9f4a6f1985fba068d611560
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-13 10:46:10 -0700

    test: model method patient alpha_sort_name

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -10,4 +10,19 @@ RSpec.describe Patient do
     it {should have_many :doctor_patients}
     it {should have_many(:doctors).through(:doctor_patients)}
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      @patient_1 = create(:patient, age: 94, name: "Billy")
+      @patient_2 = create(:patient, age: 10)
+      @patient_3 = create(:patient, age: 56, name: "Penelope")
+      @patient_4 = create(:patient, age: 42, name: "Jimmy")
+      @patient_5 = create(:patient, age: 5)
+      @patient_6 = create(:patient, age: 22, name: "Qix")
+    end
+
+    describe '#alpha_sort_name' do
+      expect(Patient.alpha_sort_name).to eq([@patient_1, @patient_4, @patient_3, @patient_6])
+    end
+  end
 end
