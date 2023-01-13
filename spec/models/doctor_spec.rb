0db1b6345627c744917242b0e106c6c0d3e327e6
Brandon Gray <brandonjoe88@hotmail.com>
2023-01-13 11:41:56 -0700

    test: doctor patient coutn method

--- spec/models/doctor_spec.rb
+++ spec/models/doctor_spec.rb
@@ -12,4 +12,33 @@ RSpec.describe Doctor do
     it {should have_many :doctor_patients}
     it {should have_many(:patients).through(:doctor_patients)}
   end
+
+  describe 'instance methods' do
+    before(:each) do
+      @hospital_1 = create(:hospital)
+      @hospital_2 = create(:hospital)
+  
+      @doctor_1 = create(:doctor, hospital: @hospital_1)
+      @doctor_2 = create(:doctor, hospital: @hospital_1)
+      @doctor_3 = create(:doctor, hospital: @hospital_1)
+      @doctor_4 = create(:doctor, hospital: @hospital_2)
+  
+      @patient_1 = create(:patient)
+      @patient_2 = create(:patient)
+      @patient_3 = create(:patient)
+      @patient_4 = create(:patient)
+      @patient_5 = create(:patient)
+  
+      @doctor_1.patients << @patient_1
+      @doctor_1.patients << @patient_2
+      @doctor_1.patients << @patient_3
+      @doctor_1.patients << @patient_4
+      @doctor_2.patients << @patient_5
+      @doctor_2.patients << @patient_1
+    end
+
+    describe '#patient_count' do
+      expect(@doctor_1.patient_count).to eq(4)
+    end
+  end
 end
